ea70e4e8989e926ec745c2aa6c616715736817fc
lil-shimon <shimon101018@gmail.com>
2025-07-07 08:50:26 +0900

    feat: add exchangeList constant to export list of exchanges

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -6,6 +6,8 @@ export const EXCHANGES = {
 
 export type Exchange = (typeof EXCHANGES)[keyof typeof EXCHANGES];
 
+export const exchangeList = Object.values(EXCHANGES) as Exchange[];
+
 export const TAKER_FEES = {
   // https://www.binance.com/en/fee/schedule
   // BNB(Binance Coin)を使用している場合は、0.075%の手数料が適用される

a10534ed0a62786203f2a3847ef014eca5da349c
lil-shimon <shimon101018@gmail.com>
2025-07-06 22:03:21 +0900

    feat: update fetchPrices to include taker fee rates in spread calculation

--- src/logic/fetch-price/fetchPrice.ts
+++ src/logic/fetch-price/fetchPrice.ts
@@ -1,11 +1,22 @@
 import { fetchBinance, fetchBybit } from "../../clients";
+import { EXCHANGES, TAKER_FEES } from "../../constants";
 import { calculateSpread, calculateProfitRate } from "../../core";
+
+const atRate = (percentage: number) => percentage / 100;
 
 export const fetchPrices = async () => {
   const binancePrice = await fetchBinance();
   const bybitPrice = await fetchBybit();
 
-  const spread = calculateSpread(binancePrice.ask, bybitPrice.bid);
+  const binanceTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BINANCE]);
+  const bybitTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BYBIT]);
+
+  const spread = calculateSpread(
+    binancePrice.ask,
+    bybitPrice.bid,
+    binanceTakerFeeRate,
+    bybitTakerFeeRate
+  );
 
   console.log("スプレッド (Binance Ask - Bybit Bid):", spread);
 

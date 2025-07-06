6cea3c43b9c3bd41f7fcc805ffdcb2413dd47f7b
lil-shimon <shimon101018@gmail.com>
2025-07-07 08:42:08 +0900

    feat: export fetchMexc function from mexc client

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,3 +1,4 @@
 export { postMessage } from "./discord/post-message";
 export { fetchBybit } from "./bybit/fetch-bybit";
 export { fetchBinance } from "./binance/fetch-binance";
+export { fetchMexc } from "./mexc/fetch-mexc";

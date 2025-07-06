accbf5379aa4c725a1220aeba728d3bdddac10ee
lil-shimon <shimon101018@gmail.com>
2025-07-07 08:41:53 +0900

    test: add unit tests for fetchPriceByPair function with mocked client responses

---
+++ src/logic/fetch-price-by-pair/fetch-price-by-pair.spec.ts
@@ -0,0 +1,37 @@
+import { fetchPriceByPair } from "./fetch-price-by-pair";
+import { describe, vi, it, expect } from "vitest";
+
+import * as clientModule from "../../clients";
+import { PAIRS } from "../../constants";
+
+describe("fetchPrices", () => {
+  vi.spyOn(clientModule, "fetchBinance").mockResolvedValue({
+    bid: 1,
+    ask: 2,
+  });
+  vi.spyOn(clientModule, "fetchBybit").mockResolvedValue({
+    bid: 1.5,
+    ask: 2.5,
+  });
+  vi.spyOn(clientModule, "fetchMexc").mockResolvedValue({
+    bid: 1.5,
+    ask: 2.5,
+  });
+  it("should return prices", async () => {
+    const result = await fetchPriceByPair(PAIRS.ADA_USDT);
+    expect(result).toEqual({
+      binance: { bid: 1, ask: 2 },
+      bybit: { bid: 1.5, ask: 2.5 },
+      mexc: { bid: 1.5, ask: 2.5 },
+    });
+  });
+
+  it("should return prices with correct types", async () => {
+    const result = await fetchPriceByPair(PAIRS.ADA_USDT);
+    expect(result).toEqual({
+      binance: { bid: expect.any(Number), ask: expect.any(Number) },
+      bybit: { bid: expect.any(Number), ask: expect.any(Number) },
+      mexc: { bid: expect.any(Number), ask: expect.any(Number) },
+    });
+  });
+});

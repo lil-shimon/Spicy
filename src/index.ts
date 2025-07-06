b9bdb5b264d2bf7b4c1af9a8b0deca710e6b5b39
lil-shimon <shimon101018@gmail.com>
2025-07-06 19:51:11 +0900

    feat: 🎉 refactor main function to utilize fetchPrices from logic module

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
-import { calculateSpread } from "./core";
-import { fetchBinance, fetchBybit } from "./clients";
+import { fetchPrices } from "./logic";
 
-const fetchPrices = async () => {
-  const binancePrice = await fetchBinance();
-  const bybitPrice = await fetchBybit();
-
-  const spread = calculateSpread(binancePrice.ask, bybitPrice.bid);
-
-  console.log("Binance Price:", binancePrice);
-  console.log("Bybit Price:", bybitPrice);
-  console.log("Spread (Binance Ask - Bybit Bid):", spread);
+const main = async () => {
+  await fetchPrices();
 };
 
-fetchPrices();
+main();
---
+++ src/logic/index.ts
@@ -0,0 +1,1 @@
+export { fetchPrices } from "./fetchPrice/fetchPrice";

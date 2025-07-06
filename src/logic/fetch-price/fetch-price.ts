2ec07ee4121e36e133ef3dab60e1e4f97460fcda
lil-shimon <shimon101018@gmail.com>
2025-07-07 08:50:32 +0900

    feat: update fetchPrices to iterate over exchangeList for price comparisons

--- src/logic/fetch-price/fetch-price.ts
+++ src/logic/fetch-price/fetch-price.ts
@@ -1,4 +1,4 @@
-import { EXCHANGES, PAIRS, TAKER_FEES } from "../../constants";
+import { exchangeList, EXCHANGES, PAIRS, TAKER_FEES } from "../../constants";
 import { calculateSpread, calculateProfitRate } from "../../core";
 import { fetchPriceByPair } from "../fetch-price-by-pair/fetch-price-by-pair";
 
@@ -13,11 +13,33 @@ export const fetchPrices = async () => {
     return 0;
   }
 
-  const { binance, bybit, mexc } = prices;
+  const { binance, bybit } = prices;
+  console.log("exchange list", exchangeList);
+
+  for (const from of exchangeList) {
+    for (const to of exchangeList) {
+      if (from === to) {
+        // 同じ取引所間の比較はスキップ
+        continue;
+      }
+
+      const spread = calculateSpread(
+        prices[from].ask,
+        prices[to].bid,
+        atRate(TAKER_FEES[from]),
+        atRate(TAKER_FEES[to])
+      );
+
+      const profitRate = calculateProfitRate(spread, prices[from].ask);
+
+      console.log(
+        `スプレッド (${from} Ask - ${to} Bid): ${spread}, 利益率: ${profitRate}`
+      );
+    }
+  }
 
   const binanceTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BINANCE]);
   const bybitTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BYBIT]);
-  const mexcTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.MEXC]);
 
   const spread = calculateSpread(
     binance.ask,

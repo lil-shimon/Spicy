37d19bb545808a19949dc296689ce7fb1b4f2726
lil-shimon <shimon101018@gmail.com>
2025-07-07 08:42:04 +0900

    feat: refactor fetchPrices to use fetchPriceByPair for price retrieval

--- src/logic/fetch-price/fetch-price.ts
+++ src/logic/fetch-price/fetch-price.ts
@@ -1,27 +1,33 @@
-import { fetchBinance, fetchBybit } from "../../clients";
 import { EXCHANGES, PAIRS, TAKER_FEES } from "../../constants";
 import { calculateSpread, calculateProfitRate } from "../../core";
+import { fetchPriceByPair } from "../fetch-price-by-pair/fetch-price-by-pair";
 
 const atRate = (percentage: number) => percentage / 100;
 
 export const fetchPrices = async () => {
   const pair = PAIRS.ADA_USDT;
-  const binancePrice = await fetchBinance(pair);
-  const bybitPrice = await fetchBybit(pair);
+
+  const prices = await fetchPriceByPair(pair);
+
+  if (!prices) {
+    return;
+  }
+
+  const { binance, bybit, mexc } = prices;
 
   const binanceTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BINANCE]);
   const bybitTakerFeeRate = atRate(TAKER_FEES[EXCHANGES.BYBIT]);
 
   const spread = calculateSpread(
-    binancePrice.ask,
-    bybitPrice.bid,
+    binance.ask,
+    bybit.bid,
     binanceTakerFeeRate,
     bybitTakerFeeRate
   );
 
   console.log("スプレッド (Binance Ask - Bybit Bid):", spread);
 
-  const profitRate = calculateProfitRate(spread, binancePrice.ask);
+  const profitRate = calculateProfitRate(spread, binance.ask);
   console.log("利益率 (スプレッド / Binance Ask):", profitRate);
 
   return profitRate;

f2a0ce5b02b25b2368a2edcbc15466b074ea893d
lil-shimon <shimon101018@gmail.com>
2025-07-08 08:23:00 +0900

    refactor: move error handling into try-catch block in fetchBinance function

--- src/clients/binance/fetch-binance.ts
+++ src/clients/binance/fetch-binance.ts
@@ -3,11 +3,19 @@ import { Pair } from "../../constants";
 
 export const fetchBinance = async (pair: Pair) => {
   const client = new binance();
-  const ticker = await client.fetchTicker(pair);
-  return {
-    // NOTE: レスポンスがNum形になっているので、nullチェックを行う
-    // もしnullの場合は0を返す
-    bid: ticker.bid ?? 0,
-    ask: ticker.ask ?? 0,
-  };
+  try {
+    const ticker = await client.fetchTicker(pair);
+    return {
+      // NOTE: レスポンスがNum形になっているので、nullチェックを行う
+      // もしnullの場合は0を返す
+      bid: ticker.bid ?? 0,
+      ask: ticker.ask ?? 0,
+    };
+  } catch (error) {
+    console.error(`binance: 価格取得に失敗しました: ${pair}`, error);
+    return {
+      bid: 0,
+      ask: 0,
+    };
+  }
 };

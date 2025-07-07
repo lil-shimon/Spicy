7a27622d6583c18ee335ba5d651e88f0ff777de5
lil-shimon <shimon101018@gmail.com>
2025-07-07 22:50:49 +0900

    feat: export fetchKucoin function from kucoin module

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -2,3 +2,4 @@ export { postMessage } from "./discord/post-message";
 export { fetchBybit } from "./bybit/fetch-bybit";
 export { fetchBinance } from "./binance/fetch-binance";
 export { fetchMexc } from "./mexc/fetch-mexc";
+export { fetchKucoin } from "./kucoin/fetch-kucoin";

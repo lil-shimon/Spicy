bb78e4dc39ce309b98961c6825bfd5f42c5cbd3d
lil-shimon <shimon101018@gmail.com>
2025-07-08 23:15:06 +0900

    refactor: update test to use PAIRS constant for HNT/USDT

--- src/logic/fetch-price/fetch-price.spec.ts
+++ src/logic/fetch-price/fetch-price.spec.ts
@@ -4,6 +4,7 @@ import { fetchPrices } from "./fetch-price";
 // import * as clientModule from "../../clients";
 import * as coreModule from "../../core";
 import * as fetchPriceModule from "../fetch-price-by-pair/fetch-price-by-pair";
+import { PAIRS } from "../../constants";
 
 describe("fetchPrices", () => {
   it("should return profit rate", async () => {
@@ -21,7 +22,7 @@ describe("fetchPrices", () => {
     expect(result).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
-          pair: "ADA/USDC",
+          pair: PAIRS.HNT_USDT,
           from: "mexc",
           to: "bybit",
           profit: 0.5,

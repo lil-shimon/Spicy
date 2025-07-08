3bf9af82ec9131ea378385aa186c9770d7efa372
lil-shimon <shimon101018@gmail.com>
2025-07-09 08:57:49 +0900

    feat: return structured orderbook data from fetchMexcOrderbook function

--- src/clients/mexc/fetch-mexc-orderbook.ts
+++ src/clients/mexc/fetch-mexc-orderbook.ts
@@ -5,4 +5,9 @@ export const fetchMexcOrderbook = async (pair: Pair) => {
   const client = new mexc();
   const orderbook = await client.fetchOrderBook(pair);
   console.log(`MEXC Orderbook for ${pair}:`, orderbook);
+
+  return {
+    bids: orderbook.bids ?? [],
+    asks: orderbook.asks ?? [],
+  };
 };

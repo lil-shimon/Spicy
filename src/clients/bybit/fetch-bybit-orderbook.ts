203899521c658195cb6e6b7ce89dde04bbaaa805
lil-shimon <shimon101018@gmail.com>
2025-07-11 16:08:57 +0900

    feat: implement fetchBybitOrderbook function to retrieve order book data

---
+++ src/clients/bybit/fetch-bybit-orderbook.ts
@@ -0,0 +1,13 @@
+import { bybit } from "ccxt";
+import { Pair } from "../../constants";
+
+export const fetchBybitOrderbook = async (pair: Pair) => {
+  const client = new bybit();
+  const orderbook = await client.fetchOrderBook(pair);
+  console.log(`Bybit Orderbook for ${pair}:`, orderbook);
+
+  return {
+    bids: orderbook.bids ?? [],
+    asks: orderbook.asks ?? [],
+  };
+};

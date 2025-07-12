3dfe5663852e5c478b6d89f031e3b889b3225c7c
lil-shimon <shimon101018@gmail.com>
2025-07-12 12:45:52 +0900

    feat: enhance demo function to calculate and log slippage rates from Mexc orderbook

--- src/demo.ts
+++ src/demo.ts
@@ -1,8 +1,21 @@
 import { fetchMexcOrderbook } from "./clients/mexc/fetch-mexc-orderbook";
 import { PAIRS } from "./constants";
+import { OrderBookEntry } from "./logic/slippage/calculate-buy-slippage-rate.types";
+import { calculateBuySlippageRate } from "./logic/slippage/calculate-buy-slippate-rate";
+import { calculateSellSlippageRate } from "./logic/slippage/calculate-sell-slippage-rate";
 
-const demo = () => {
-  fetchMexcOrderbook(PAIRS.HNT_USDT);
+const demo = async () => {
+  const response = await fetchMexcOrderbook(PAIRS.HNT_USDT);
+  const slippage = calculateBuySlippageRate(
+    response.asks as OrderBookEntry[],
+    10
+  );
+  console.log("スリッページ率:", slippage);
+  const sellSlippage = calculateSellSlippageRate(
+    response.bids as OrderBookEntry[],
+    10
+  );
+  console.log("売りスリッページ率:", sellSlippage);
 };
 
 demo();

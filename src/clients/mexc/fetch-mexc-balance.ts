dd6c9303dfa1c1611b5e31ca0f0e3c172cb6eaeb
lil-shimon <shimon101018@gmail.com>
2025-07-12 12:50:26 +0900

    feat: implement fetchMexcBalance function to retrieve and log MEXC account balance

---
+++ src/clients/mexc/fetch-mexc-balance.ts
@@ -0,0 +1,21 @@
+import { mexc } from "ccxt";
+import "dotenv/config";
+
+const apiKey = process.env.MEXC_API_KEY;
+const secret = process.env.MEXC_SECRET;
+
+export const fetchMexcBalance = async () => {
+  if (!apiKey || !secret) {
+    console.log("api key:", apiKey);
+    console.log("secret:", secret);
+    console.error("MEXC APIキーまたはシークレットが設定されていません。");
+    return;
+  }
+  const client = new mexc({
+    apiKey: apiKey || "",
+    secret: secret || "",
+  });
+
+  const balance = await client.fetchBalance();
+  console.log("MEXC Balance:", balance);
+};

6a6727414381bbf67c70747a5cf593a80d16ae0d
lil-shimon <shimon101018@gmail.com>
2025-07-14 08:09:15 +0900

    refactor: simplify fetchMexcBalance by using mexcClient directly

--- src/clients/mexc/fetch-mexc-balance.ts
+++ src/clients/mexc/fetch-mexc-balance.ts
@@ -1,22 +1,7 @@
-import { mexc } from 'ccxt';
 import 'dotenv/config';
-
-const apiKey = process.env.MEXC_API_KEY;
-const secret = process.env.MEXC_SECRET;
+import { mexcClient } from './mexc-client';
 
 export const fetchMexcBalance = async () => {
-  if (!apiKey || !secret) {
-    console.log('api key:', apiKey);
-    console.log('secret:', secret);
-    console.error('MEXC APIキーまたはシークレットが設定されていません。');
-    return;
-  }
-
-  const client = new mexc({
-    apiKey: apiKey || '',
-    secret: secret || '',
-  });
-
-  const balance = await client.fetchBalance();
+  const balance = await mexcClient.fetchBalance();
   console.log('MEXC Balance:', balance);
 };

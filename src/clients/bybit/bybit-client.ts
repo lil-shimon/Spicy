37acd016a7d86d71b828bf50798928e448c38185
lil-shimon <shimon101018@gmail.com>
2025-07-13 20:15:26 +0900

    feat: implement bybit client for API interaction with error handling

---
+++ src/clients/bybit/bybit-client.ts
@@ -0,0 +1,17 @@
+import { bybit } from "ccxt";
+import "dotenv/config";
+
+const apiKey = process.env.BYBIT_API_KEY;
+const secret = process.env.BYBIT_SECRET;
+
+if (!apiKey || !secret) {
+  console.log("api key:", apiKey);
+  console.log("secret:", secret);
+  console.error("Bybit APIキーまたはシークレットが設定されていません。");
+  throw new Error("Bybit APIキーまたはシークレットが設定されていません。");
+}
+
+export const bybitClient = new bybit({
+  apiKey: apiKey || "",
+  secret: secret || "",
+});

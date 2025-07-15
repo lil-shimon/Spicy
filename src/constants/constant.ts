968a0f4068b2973b6d0fdfde51ca73aab4ceba7b
lil-shimon <shimon101018@gmail.com>
2025-07-16 07:56:50 +0900

    feat: PUMP_USDTを取引ペアに追加

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -31,6 +31,7 @@ export const PAIRS = {
   // HNT_USDT: 'HNT/USDT', // MEXCのAPIが対応していないため除外
   // WLD_USDT: "WLD/USDT",
   SOL_USDT: 'SOL/USDT',
+  PUMP_USDT: 'PUMP/USDT',
 } as const;
 
 export type Pair = (typeof PAIRS)[keyof typeof PAIRS];

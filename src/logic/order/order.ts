8451590f42d482083c729abd329de902e0a309e2
lil-shimon <shimon101018@gmail.com>
2025-07-18 08:41:39 +0900

    feat: XO_DEFAULT_AMOUNT定数を追加し、createOrders関数内の注文金額を一元管理するように変更

--- src/logic/order/order.ts
+++ src/logic/order/order.ts
@@ -2,6 +2,8 @@ import { createBybitOrder, createMexcOrder } from '../../clients';
 import { EXCHANGES, PAIRS } from '../../constants';
 import { FetchPriceResult } from '../fetch-price/fetch-price';
 
+const XO_DEFAULT_AMOUNT = 1000;
+
 export const createOrders = async (data: FetchPriceResult[]) => {
   const promises = [];
 
@@ -10,20 +12,20 @@ export const createOrders = async (data: FetchPriceResult[]) => {
     if (pair === PAIRS.XO_USDT) {
       if (from === EXCHANGES.BYBIT) {
         console.log(`Bybitで買い注文を作成します ${pair} `);
-        const promise = createBybitOrder(pair, 'buy', 1000);
+        const promise = createBybitOrder(pair, 'buy', XO_DEFAULT_AMOUNT);
         promises.push(promise);
       } else if (from === EXCHANGES.MEXC) {
         console.log(`MEXCで買い注文を作成します ${pair} `);
-        const promise = createMexcOrder(pair, 'buy', 1000);
+        const promise = createMexcOrder(pair, 'buy', XO_DEFAULT_AMOUNT);
         promises.push(promise);
       }
       if (to === EXCHANGES.BYBIT) {
         console.log(`Bybitで売り注文を作成します ${pair} `);
-        const promise = createBybitOrder(pair, 'sell', 1000);
+        const promise = createBybitOrder(pair, 'sell', XO_DEFAULT_AMOUNT);
         promises.push(promise);
       } else if (to === EXCHANGES.MEXC) {
         console.log(`MEXCで売り注文を作成します ${pair} `);
-        const promise = createMexcOrder(pair, 'sell', 1000);
+        const promise = createMexcOrder(pair, 'sell', XO_DEFAULT_AMOUNT);
         promises.push(promise);
       }
     }

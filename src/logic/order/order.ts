35d7a6a869e383fe8f2c4140f714f4bd56cf81e8
lil-shimon <shimon101018@gmail.com>
2025-07-18 08:35:37 +0900

    feat: createOrders関数からDiscordへの通知機能を削除し、コンソールログのみに変更

--- src/logic/order/order.ts
+++ src/logic/order/order.ts
@@ -1,7 +1,6 @@
 import { createBybitOrder, createMexcOrder } from '../../clients';
 import { EXCHANGES, PAIRS } from '../../constants';
 import { FetchPriceResult } from '../fetch-price/fetch-price';
-import { postMessage } from '../../clients/discord/post-message';
 
 export const createOrders = async (data: FetchPriceResult[]) => {
   const promises = [];
@@ -13,23 +12,19 @@ export const createOrders = async (data: FetchPriceResult[]) => {
         console.log(`Bybitで買い注文を作成します ${pair} `);
         const promise = createBybitOrder(pair, 'buy', 1000);
         promises.push(promise);
-        postMessage(`Bybitで買い注文を作成します ${pair} `);
       } else if (from === EXCHANGES.MEXC) {
         console.log(`MEXCで買い注文を作成します ${pair} `);
         const promise = createMexcOrder(pair, 'buy', 1000);
         promises.push(promise);
-        postMessage(`MEXCで買い注文を作成します ${pair} `);
       }
       if (to === EXCHANGES.BYBIT) {
         console.log(`Bybitで売り注文を作成します ${pair} `);
         const promise = createBybitOrder(pair, 'sell', 1000);
         promises.push(promise);
-        postMessage(`Bybitで売り注文を作成します ${pair} `);
       } else if (to === EXCHANGES.MEXC) {
         console.log(`MEXCで売り注文を作成します ${pair} `);
         const promise = createMexcOrder(pair, 'sell', 1000);
         promises.push(promise);
-        postMessage(`MEXCで売り注文を作成します ${pair} `);
       }
     }
   }

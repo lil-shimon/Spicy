ea33da13b85f6318eba03695e5f21fdf76692eec
lil-shimon <shimon101018@gmail.com>
2025-07-18 08:21:50 +0900

    feat: MEXC注文作成時の成功とエラーをDiscordに通知する機能を追加

--- src/clients/mexc/create-mexc-order.ts
+++ src/clients/mexc/create-mexc-order.ts
@@ -1,4 +1,5 @@
 import { Pair } from '../../constants';
+import { postMessage } from '../discord/post-message';
 import { mexcClient } from './mexc-client';
 
 export const createMexcOrder = async (
@@ -9,9 +10,13 @@ export const createMexcOrder = async (
   try {
     const order = await mexcClient.createOrder(pair, 'market', side, amount);
     console.log('MEXCでの注文が成功しました:', order);
+    await postMessage(`MEXCでの注文が成功しました: ${JSON.stringify(order)}`);
     return order;
   } catch (error) {
     console.error('MEXCでの注文作成中にエラーが発生しました:', error);
+    await postMessage(
+      `MEXCでの注文作成中にエラーが発生しました: ${JSON.stringify(error)}`
+    );
     throw error;
   }
 };

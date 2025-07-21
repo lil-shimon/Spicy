972ea6e8b5c9c5f03e7be02af6f4033d27f6e3ef
lil-shimon <shimon101018@gmail.com>
2025-07-21 23:36:15 +0900

    refactor: 💡 update fetchBinance to binanceClient

--- src/clients/binance/fetch-binance.ts
+++ src/clients/binance/fetch-binance.ts
@@ -1,10 +1,9 @@
-import { binance } from 'ccxt';
 import { Pair } from '../../constants';
+import { binanceClient } from './binance-client';
 
 export const fetchBinance = async (pair: Pair) => {
-  const client = new binance();
   try {
-    const ticker = await client.fetchTicker(pair);
+    const ticker = await binanceClient.fetchTicker(pair);
     return {
       // NOTE: レスポンスがNum形になっているので、nullチェックを行う
       // もしnullの場合は0を返す

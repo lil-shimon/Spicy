d297bb6c5d2a7efb68d1b357e975de2466756743
lil-shimon <shimon101018@gmail.com>
2025-07-21 23:56:03 +0900

    feat: 🎸 add XO_USDT error handling

--- src/clients/binance/fetch-binance.ts
+++ src/clients/binance/fetch-binance.ts
@@ -1,7 +1,17 @@
-import { Pair } from '../../constants';
+import { Pair, PAIRS } from '../../constants';
 import { binanceClient } from './binance-client';
 
 export const fetchBinance = async (pair: Pair) => {
+  // XO_USDTはBinanceでは取扱いがないため、0を返す
+  // 本当はこの処理は他の箇所で行いたい
+  // TODO: refactorして、XO_USDTの処理を他の箇所に移動する
+  if (pair === PAIRS.XO_USDT) {
+    return {
+      bid: 0,
+      ask: 0,
+    };
+  }
+
   try {
     const ticker = await binanceClient.fetchTicker(pair);
     return {

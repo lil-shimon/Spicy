9b1c85c3f2ca5b25133a5633eb81993906fdd943
lil-shimon <shimon101018@gmail.com>
2025-07-26 23:25:58 +0900

    exchange-arb.tsにアービトラージのスプレッド計算機能を追加し、GMOとMEXCの価格差を評価するロジックを実装しました。スプレッドが閾値を超えた場合にアラートメッセージを送信する機能も追加しました。

--- src/exchange-arb.ts
+++ src/exchange-arb.ts
@@ -1,11 +1,15 @@
+import { postMessage } from './clients';
 import { fetchGmoAskBid, fetchJpyUsd } from './clients/gmo/gmo';
-import { fetchAskBid } from './clients/mexc/usdc-usdt';
+import { calcSpreadRate, fetchAskBid } from './clients/mexc/usdc-usdt';
 
 export const exchangeArb = async () => {
   // 1. 各取引所の価格を取得 (最初はGMO, MEXC)
+  // 2. GMOはSOL/JPY, MEXCはSOL/USDT
   const gmoSolJpy = await fetchGmoAskBid('SOL');
-  const jpyUsd = await fetchJpyUsd();
   const mexcSolUsdt = await fetchAskBid('SOL/USDT');
+  // 3. 為替情報を取得(JPY/USD)
+  const jpyUsd = await fetchJpyUsd();
+
   if (
     !gmoSolJpy ||
     !mexcSolUsdt?.ask ||
@@ -17,15 +21,32 @@ export const exchangeArb = async () => {
     return;
   }
 
-  // 2. GMOはSOL/JPY, MEXCはSOL/USDT
-  // 3. 為替情報を取得(JPY/USD)
   // 4. 為替情報を使って、MEXCのSOL/USDTをJPYに変換
-  const gmoSolJpyBid = gmoSolJpy.bid;
-  const gmoSolJpyAsk = gmoSolJpy.ask;
   const mexcSolUsdtBid = mexcSolUsdt.bid * jpyUsd.ask;
   const mexcSolUsdtAsk = mexcSolUsdt.ask * jpyUsd.bid;
+
   // 5. 変換後の価格をGMOのSOL/JPYと比較
-  // 6. 差益があれば、注文を出す
+  const gmoBuySpreadRate = calcSpreadRate(gmoSolJpy.ask, mexcSolUsdtBid);
+  const gmoSellSpreadRate = calcSpreadRate(mexcSolUsdtAsk, gmoSolJpy.bid);
+
+  console.log('スプレッド（SOLをJPYで買ってUSDTで売る）', gmoBuySpreadRate);
+  console.log('スプレッド（SOLをUSDTで買ってJPYで売る）', gmoSellSpreadRate);
+
+  // 6. 差益があるかの判定
+  const threshold = 0.1;
+
+  // 7. TODO: 差益があれば、注文を出す
+  if (gmoBuySpreadRate > threshold) {
+    const message = `💰 GMO買→MEXC売アービトラージのチャンス！ スプレッド: ${gmoBuySpreadRate}% `;
+    console.log(message);
+    await postMessage(message);
+  }
+
+  if (gmoSellSpreadRate > threshold) {
+    const message = `💰 MEXC買→GMO売アービトラージのチャンス！ スプレッド: ${gmoSellSpreadRate}% `;
+    console.log(message);
+    await postMessage(message);
+  }
 };
 
 exchangeArb();

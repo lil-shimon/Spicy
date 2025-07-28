4f42cab36d613358e753411bfd0f4ef7529a108f
lil-shimon <shimon101018@gmail.com>
2025-07-28 18:09:39 +0900

    startDirtyWork関数にCoinw接続の更新処理を追加し、最良の入札と売値をログ出力するようにしました。

--- src/dirty-work/bot.ts
+++ src/dirty-work/bot.ts
@@ -1,3 +1,19 @@
+import { connectCoinw } from '../clients/coinw/coinw-ws';
+
+const handleUpdate = (bestBid: number, bestAsk: number) => {
+  console.log('Best Bid:', bestBid);
+  console.log('Best Ask:', bestAsk);
+};
+
 export const startDirtyWork = async () => {
   console.log('MMBot start');
+  connectCoinw({
+    onUpdate: handleUpdate,
+    onError: (error) => {
+      console.error('Coinw error:', error);
+    },
+    onClose: () => {
+      console.log('Coinw connection closed');
+    },
+  });
 };

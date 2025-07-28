0b74a14145f73cb0b86eb0d894d81c442bd9b6f6
lil-shimon <shimon101018@gmail.com>
2025-07-28 23:26:10 +0900

    handleEnableOrder関数を更新し、注文作成時のエラーハンドリングを追加しました。また、スプレッドが閾値を超えた場合の処理を改善し、シンボルを引数として受け取るようにしました。startDirtyWork関数もシンボルを引数に取るように変更し、最良の入札と売値を取得する処理を追加しました。

--- src/dirty-work/bot.ts
+++ src/dirty-work/bot.ts
@@ -1,44 +1,112 @@
-import { connectCoinw } from '../clients/coinw/coinw-ws';
 import { postMMMessage } from '../clients/discord/post-message';
+import { fetchMexcOrder } from '../clients/mexc/create-mexc-order';
+import { fetchMexcOrderbook } from '../clients/mexc/fetch-mexc-orderbook';
+import { orderLimit } from '../clients/mexc/usdc-usdt';
+import { Pair } from '../constants';
 import { calculateSpreadRate } from './spread';
 
 const spreadThreshold = 0.1; // スプレッドの閾値を設定
+const TICK = 0.0001;
+const FULL_SPREAD = 0.0001;
+const HALF = FULL_SPREAD / 2;
+
+/**
+ * TICK の倍数に切り捨て
+ * roundDown(99.9517)  // → 99.951
+ */
+const roundDown = (price: number, tick = TICK) => {
+  return Math.floor(price / tick) * tick;
+};
+
+/**
+ * TICK の倍数に切り上げ
+ * roundUp  (99.9517)  // → 99.952
+ */
+const roundUp = (price: number, tick = TICK) => {
+  return Math.ceil(price / tick) * tick;
+};
 
 const handleEnableOrder = async (
   bestBid: number,
   bestAsk: number,
-  spreadRate: number
+  spreadRate: number,
+  symbol: string
 ) => {
-  const now = new Date().toLocaleDateString('ja-JP', {
-    timeZone: 'Asia/Tokyo',
-  });
-  const message = `[DirtyWork] スプレッドが ${spreadThreshold}% を超えました。現在のスプレッド: ${spreadRate}%, ベストビッド: ${bestBid}, ベストアスク: ${bestAsk} 時刻: ${now}`;
-  // TODO: 毎回通知するとToo Many Requestsになる。
-  // どのタイミングでどのようなメッセー通知通知するかを検討する
-  await postMMMessage(message);
-};
+  const mid = (bestBid + bestAsk) / 2;
 
-const handleUpdate = (bestBid: number, bestAsk: number) => {
-  console.log('Best Bid:', bestBid);
-  console.log('Best Ask:', bestAsk);
-  const spread = calculateSpreadRate(bestBid, bestAsk);
-  console.log('Spread:', spread);
+  const buyPrice = roundDown(mid * (1 - HALF), TICK);
+  const sellPrice = roundUp(mid * (1 + HALF), TICK);
 
-  if (spread > spreadThreshold) {
-    console.log('Spread is above threshold, taking action...');
-    handleEnableOrder(bestBid, bestAsk, spread);
+  console.log('buyPrice', buyPrice);
+  console.log('sellPrice', sellPrice);
+
+  const [buyOrder, sellOrder] = await Promise.all([
+    orderLimit(symbol, 'buy', buyPrice, 0.001),
+    orderLimit(symbol, 'sell', sellPrice, 0.001),
+  ]);
+
+  console.log('buyOrder', buyOrder);
+  console.log('sellOrder', sellOrder);
+  const [buyOrderId, sellOrderId] = [buyOrder?.id, sellOrder?.id];
+
+  if (!buyOrderId || !sellOrderId) {
+    await postMMMessage(
+      `[DirtyWork] 注文作成中にエラーが発生しました?? (IDが取得できない): ${JSON.stringify(
+        {
+          buyOrder,
+          sellOrder,
+        }
+      )}`
+    );
+    return;
+  }
+
+  const tStart = Date.now();
+  const TIMEOUT_MS = 1000 * 60;
+
+  while (Date.now() - tStart < TIMEOUT_MS) {
+    const [buyOrder, sellOrder] = await Promise.all([
+      fetchMexcOrder(buyOrderId),
+      fetchMexcOrder(sellOrderId),
+    ]);
+
+    console.log('buyOrder', buyOrder);
+    console.log('sellOrder', sellOrder);
+
+    if (buyOrder?.status === 'open' && sellOrder?.status === 'open') {
+      break;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 2_000));
   }
 };
 
-export const startDirtyWork = async () => {
+const handleUpdate = (bestBid: number, bestAsk: number, symbol: string) => {
+  const spread = calculateSpreadRate(bestBid, bestAsk);
+
+  if (spread < spreadThreshold) {
+    console.log(
+      'スプレッドが閾値以下になったので、何もしません',
+      spread,
+      bestBid,
+      bestAsk
+    );
+    return;
+  }
+
+  handleEnableOrder(bestBid, bestAsk, spread, symbol);
+};
+
+export const startDirtyWork = async (symbol: string) => {
   console.log('MMBot start');
-  connectCoinw({
-    onUpdate: handleUpdate,
-    onError: (error) => {
-      console.error('Coinw error:', error);
-    },
-    onClose: () => {
-      console.log('Coinw connection closed');
-    },
-  });
+  const response = await fetchMexcOrderbook(symbol as Pair);
+
+  const bestBid = response.bids[0][0];
+  const bestAsk = response.asks[0][0];
+
+  if (!bestBid || !bestAsk) {
+    return;
+  }
+
+  handleUpdate(bestBid, bestAsk, symbol);
 };

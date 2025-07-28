5c69e1ba02dfe54a7cdd0612e8888bcbfbf2ecd7
lil-shimon <shimon101018@gmail.com>
2025-07-28 22:42:28 +0900

    fetchMexcOrderbook関数からデバッグ用のconsole.logを削除しました。

--- src/clients/mexc/fetch-mexc-orderbook.ts
+++ src/clients/mexc/fetch-mexc-orderbook.ts
@@ -3,7 +3,6 @@ import { mexcClient } from './mexc-client';
 
 export const fetchMexcOrderbook = async (pair: Pair) => {
   const orderbook = await mexcClient.fetchOrderBook(pair);
-  console.log(`MEXC Orderbook for ${pair}:`, orderbook);
 
   return {
     bids: orderbook.bids ?? [],

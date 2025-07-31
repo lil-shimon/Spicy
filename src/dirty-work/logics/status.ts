afeda79ee5b1a2ffcf75e5090277371eee7d9829
lil-shimon <shimon101018@gmail.com>
2025-07-31 23:58:10 +0900

    handleStatus関数に注文のステータスがすべて閉じているかどうかを確認するロジックを追加しました。これにより、関数が返すオブジェクトにisClosedプロパティが含まれるようになり、ステータスの管理がより柔軟になりました。

--- src/dirty-work/logics/status.ts
+++ src/dirty-work/logics/status.ts
@@ -6,5 +6,6 @@ type HandleStatusParams = {
 
 export const handleStatus = ({ orders }: HandleStatusParams) => {
   const isOpened = orders.some((order) => order?.status === 'open');
-  return { isOpened };
+  const isClosed = orders.every((order) => order?.status === 'closed');
+  return { isOpened, isClosed };
 };

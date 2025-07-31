9c5d3362e0457b87569cd438aa3cefccb6c2b914
lil-shimon <shimon101018@gmail.com>
2025-07-31 23:58:21 +0900

    handleStatus関数のテストを整理し、isOpenedおよびisClosedの状態を確認する新しいテストケースを追加しました。これにより、注文のステータスに基づくロジックの検証が強化され、テストのカバレッジが向上しました。

--- src/dirty-work/logics/status.spec.ts
+++ src/dirty-work/logics/status.spec.ts
@@ -3,21 +3,43 @@ import { handleStatus } from './status';
 import { Order } from 'ccxt';
 
 describe('handleStatus', () => {
-  it('should return true if any order is open', () => {
-    const orders = [{ status: 'open' }, { status: 'closed' }] as Order[];
-    const { isOpened } = handleStatus({ orders });
-    expect(isOpened).toBe(true);
+  describe('isOpened', () => {
+    it('should return true if any order is open', () => {
+      const orders = [{ status: 'open' }, { status: 'closed' }] as Order[];
+      const { isOpened } = handleStatus({ orders });
+      expect(isOpened).toBe(true);
+    });
+
+    it('should return false if no order is open', () => {
+      const orders = [{ status: 'closed' }, { status: 'closed' }] as Order[];
+      const { isOpened } = handleStatus({ orders });
+      expect(isOpened).toBe(false);
+    });
+
+    it('should return false if one order is open', () => {
+      const orders = [{ status: 'open' }, { status: 'closed' }] as Order[];
+      const { isOpened } = handleStatus({ orders });
+      expect(isOpened).toBe(true);
+    });
   });
 
-  it('should return false if no order is open', () => {
-    const orders = [{ status: 'closed' }, { status: 'closed' }] as Order[];
-    const { isOpened } = handleStatus({ orders });
-    expect(isOpened).toBe(false);
-  });
+  describe('isClosed', () => {
+    it('should return true if all orders are closed', () => {
+      const orders = [{ status: 'closed' }, { status: 'closed' }] as Order[];
+      const { isClosed } = handleStatus({ orders });
+      expect(isClosed).toBe(true);
+    });
 
-  it('should return false if one order is open', () => {
-    const orders = [{ status: 'open' }, { status: 'closed' }] as Order[];
-    const { isOpened } = handleStatus({ orders });
-    expect(isOpened).toBe(true);
+    it('should return false if one order is open', () => {
+      const orders = [{ status: 'open' }, { status: 'closed' }] as Order[];
+      const { isClosed } = handleStatus({ orders });
+      expect(isClosed).toBe(false);
+    });
+
+    it('should return false if no order is open', () => {
+      const orders = [{ status: 'closed' }, { status: 'closed' }] as Order[];
+      const { isClosed } = handleStatus({ orders });
+      expect(isClosed).toBe(true);
+    });
   });
 });

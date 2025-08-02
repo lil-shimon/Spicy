837e63b1e6fcec2566d520a6452e57b21f1cd222
lil-shimon <shimon101018@gmail.com>
2025-08-03 02:28:17 +0900

    getInventoryメソッドのログ出力を改善し、在庫のシンボルをコンソールに表示するように変更しました。

--- src/dirty-work/services/inventory-service.ts
+++ src/dirty-work/services/inventory-service.ts
@@ -15,7 +15,7 @@ export class InventoryService {
 
   getInventory = (pair: string) => {
     const symbol = pair.split('/')[0];
-    console.log('inventory', this.inventory[symbol]);
+    console.log('inventory', this.inventory[symbol], symbol);
     return this.inventory[symbol] ?? 0;
   };
 }

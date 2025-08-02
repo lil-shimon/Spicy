ac4efc9a9bad2dd5cda114b5091090d5fa40ee4c
lil-shimon <shimon101018@gmail.com>
2025-08-03 02:42:49 +0900

    PnLServiceにcurrentPriceプロパティを追加し、updatePnlメソッドを修正してトークン価格の取得方法を変更しました。また、currentPriceを更新するためのupdateCurrentPriceメソッドを新たに実装しました。

--- src/dirty-work/services/pnl-service.ts
+++ src/dirty-work/services/pnl-service.ts
@@ -4,20 +4,25 @@ export class PnLService {
   private prevUSDT = 0;
   private pnl = 0;
   private inventoryService: InventoryService;
+  private currentPrice: number = 0;
 
   constructor(inventoryService: InventoryService) {
     this.inventoryService = inventoryService;
   }
 
-  updatePnl = (token: string, stable: string, tokenPrice: number) => {
+  updatePnl = (token: string, stable: string) => {
     const inventoryToken = this.inventoryService.getInventory(token);
     const inventoryStable = this.inventoryService.getInventory(stable);
-    const tokenToStable = inventoryToken * tokenPrice;
+    const tokenToStable = inventoryToken * this.currentPrice;
     this.prevUSDT = tokenToStable + inventoryStable;
     this.pnl = this.prevUSDT - this.pnl;
   };
 
   getPnl = () => {
     return this.pnl;
+  };
+
+  updateCurrentPrice = (price: number) => {
+    this.currentPrice = price;
   };
 }

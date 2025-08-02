6a08475d29093e6a2e82899391bfcf722a851d71
lil-shimon <shimon101018@gmail.com>
2025-08-02 20:43:07 +0900

    OrderServiceに新たにordersプロパティを追加し、addOrderおよびremoveOrderメソッドを実装しました。これにより、注文の追加と削除が可能になりました。

--- src/dirty-work/order-service.ts
+++ src/dirty-work/order-service.ts
@@ -1,7 +1,20 @@
+import { Order } from 'ccxt';
+
 export class OrderService {
   public ordered: boolean = false;
+  public orders: Order[] = [];
 
   updateOrderStatus = (status: boolean) => {
     this.ordered = status;
+  };
+
+  addOrder = (orders: Order[]) => {
+    this.orders.push(...orders);
+    console.log('addOrder', this.orders.length);
+  };
+
+  removeOrder = (id: string) => {
+    this.orders = this.orders.filter((o) => o.id !== id);
+    console.log('removeOrder', this.orders.length);
   };
 }

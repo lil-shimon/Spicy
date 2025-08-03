8dc463c44c65ce5d5700d0664fe895089686d0e4
lil-shimon <shimon101018@gmail.com>
2025-08-03 22:25:07 +0900

    fetchKucoinOHLCV関数にパラメータの説明を追加しました。これにより、関数の使い方が明確になります。

--- src/clients/kucoin/fetch-kucoin-ohlcv.ts
+++ src/clients/kucoin/fetch-kucoin-ohlcv.ts
@@ -1,5 +1,12 @@
 import { kucoinFuturesClient } from './kucoin-client';
 
+/**
+ *
+ * @param symbol
+ * @param timeframe
+ * @param limit
+ * @returns [ timestamp, open, high, low, close, volume ]
+ */
 export const fetchKucoinOHLCV = async (
   symbol: string,
   timeframe: string,

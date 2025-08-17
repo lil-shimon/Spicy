03e45727c66d4f7f59dd07e68d358097af3fa8e5
lil-shimon <shimon101018@gmail.com>
2025-08-17 22:01:10 +0900

    feat: 🎸 update gmo ws client

--- src/clients/gmo/gmo-ws.ts
+++ src/clients/gmo/gmo-ws.ts
@@ -3,28 +3,40 @@ import { generateSubscribeMessage } from './generate-subscribe-message';
 
 const ws = new WebSocket('wss://api.coin.z.com/ws/public/v1');
 
-ws.on('open', () => {
-  const message = generateSubscribeMessage({
-    symbol: 'BTC',
-    channel: 'ticker',
+type GmoWebSocketClientParams = {
+  onUpdate: (bid: number, ask: number) => {};
+};
+
+export const gmoWebSocketClient = (params: GmoWebSocketClientParams) => {
+  const { onUpdate } = params;
+
+  ws.on('open', () => {
+    const message = generateSubscribeMessage({
+      symbol: 'BTC',
+      channel: 'ticker',
+    });
+    ws.send(message);
   });
-  ws.send(message);
-});
 
-ws.on('message', (data) => {
-  const message = JSON.parse(data.toString('utf-8'));
-  const bid = message?.bid;
-  const ask = message?.ask;
+  ws.on('message', (data) => {
+    const message = JSON.parse(data.toString('utf-8'));
+    const bid = message?.bid;
+    const ask = message?.ask;
 
-  console.log('GMO WebSocket message received:', message);
-  console.log('bid: ', bid);
-  console.log('ask: ', ask);
-});
+    console.log('GMO WebSocket message received:', message);
+    console.log('bid: ', bid);
+    console.log('ask: ', ask);
 
-ws.on('error', (error) => {
-  console.error('GMO WebSocket error:', error);
-});
+    if (bid && ask) {
+      onUpdate(bid, ask);
+    }
+  });
 
-ws.on('close', () => {
-  console.log('GMO WebSocket connection closed');
-});
+  ws.on('error', (error) => {
+    console.error('GMO WebSocket error:', error);
+  });
+
+  ws.on('close', () => {
+    console.log('GMO WebSocket connection closed');
+  });
+};

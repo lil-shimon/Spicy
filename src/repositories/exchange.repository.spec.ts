7cd0e33c5be9e790e8c659677988f455fb00e373
lil-shimon <shimon101018@gmail.com>
2025-08-19 22:47:33 +0900

    test: 💍 update ExchangeRepository tests to use mock exchange rates

--- src/repositories/exchange.repository.spec.ts
+++ src/repositories/exchange.repository.spec.ts
@@ -8,9 +8,16 @@ describe('ExchangeRepository', () => {
   });
 
   it('should return updated exchange rate', () => {
-    exchangeRepository.updateExchangeRate(1.5);
-    expect(exchangeRepository.getExchangeRate()).toBe(1.5);
-    exchangeRepository.updateExchangeRate(2.0);
-    expect(exchangeRepository.getExchangeRate()).toBe(2.0);
+    const mockExchangeRate = {
+      bid: 1,
+      ask: 2,
+    };
+    exchangeRepository.updateExchangeRate(mockExchangeRate);
+    expect(exchangeRepository.getExchangeRate()).toEqual(mockExchangeRate);
+    exchangeRepository.updateExchangeRate({ bid: 2.0, ask: 3.0 });
+    expect(exchangeRepository.getExchangeRate()).toEqual({
+      bid: 2.0,
+      ask: 3.0,
+    });
   });
 });

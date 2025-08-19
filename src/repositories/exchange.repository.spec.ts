22b1d211074cb06f6e25a089e4a913344b36232c
lil-shimon <shimon101018@gmail.com>
2025-08-19 09:10:17 +0900

    test: 💍 update exchange repository test

--- src/repositories/exchange.repository.spec.ts
+++ src/repositories/exchange.repository.spec.ts
@@ -2,13 +2,12 @@ import { describe, expect, it } from 'vitest';
 import { ExchangeRepository } from './exchange.repository';
 
 describe('ExchangeRepository', () => {
+  const exchangeRepository = ExchangeRepository();
   it('should return initial exchange rate', () => {
-    const exchangeRepository = ExchangeRepository();
     expect(exchangeRepository.getExchangeRate()).toBe(0);
   });
 
   it('should return updated exchange rate', () => {
-    const exchangeRepository = ExchangeRepository();
     exchangeRepository.updateExchangeRate(1.5);
     expect(exchangeRepository.getExchangeRate()).toBe(1.5);
     exchangeRepository.updateExchangeRate(2.0);

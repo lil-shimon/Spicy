59385b351736f16c1c2e63976d198b2a5e7f773c
lil-shimon <shimon101018@gmail.com>
2025-08-21 08:37:59 +0900

    feat: 🎸 add check params

--- src/services/arbitrage.service.ts
+++ src/services/arbitrage.service.ts
@@ -1,5 +1,12 @@
+type CheckParams = {
+  exchangeA: { ask: number; bid: number };
+  exchangeB: { ask: number; bid: number };
+};
+
 export const ArbitrageService = () => {
-  const check = () => {};
+  const check = (params: CheckParams) => {
+    const { exchangeA, exchangeB } = params;
+  };
 
   return { check } as const;
 };

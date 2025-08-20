4860e366a10a0c384579e07185aaa2bd4b262567
lil-shimon <shimon101018@gmail.com>
2025-08-21 08:46:21 +0900

    feat: 🎸 compare

--- src/services/arbitrage.service.ts
+++ src/services/arbitrage.service.ts
@@ -6,6 +6,14 @@ type CheckParams = {
 export const ArbitrageService = () => {
   const check = (params: CheckParams) => {
     const { exchangeA, exchangeB } = params;
+    // 指値でMMっぽくする時の比較
+    const aToB = compare({ buyPrice: exchangeA.bid, sellPrice: exchangeB.ask });
+    const bToA = compare({ buyPrice: exchangeB.bid, sellPrice: exchangeA.ask });
+
+    return {
+      aToB,
+      bToA,
+    };
   };
 
   const compare = ({

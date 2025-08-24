70f7bef09e28f9fdfcf3d5e2f55e2e47baa44c36
lil-shimon <shimon101018@gmail.com>
2025-08-24 11:21:54 +0900

    feat: 🎸 add getMakerFeeSpot

--- src/core/maker-fee/maker-fee.ts
+++ src/core/maker-fee/maker-fee.ts
@@ -1,4 +1,4 @@
-import { Exchange, MAKER_FEES_FUTURES } from '../../constants';
+import { Exchange, MAKER_FEES_FUTURES, MAKER_FEES_SPOT } from '../../constants';
 
 export const getMakerFeeFutures = (exchange: Exchange) => {
   const makerFee =
@@ -7,6 +7,16 @@ export const getMakerFeeFutures = (exchange: Exchange) => {
     throw new Error(
       `Maker fee for futures exchange ${exchange} is not defined.`
     );
+  }
+
+  return makerFee;
+};
+
+export const getMakerFeeSpot = (exchange: Exchange) => {
+  const makerFee = MAKER_FEES_SPOT[exchange as keyof typeof MAKER_FEES_SPOT];
+
+  if (makerFee === undefined) {
+    throw new Error(`Maker fee for spot exchange ${exchange} is not defined.`);
   }
 
   return makerFee;

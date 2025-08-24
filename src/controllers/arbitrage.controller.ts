a5058aff5c3543996e1039e2572590036dc37320
lil-shimon <shimon101018@gmail.com>
2025-08-24 10:31:53 +0900

    refactor: 💡 remove TODO comment

--- src/controllers/arbitrage.controller.ts
+++ src/controllers/arbitrage.controller.ts
@@ -4,10 +4,6 @@ import { ArbitrageService } from '../services/arbitrage.service';
 import { ExchangeRateService } from '../services/exchange-rate.service';
 import { PriceService } from '../services/price.service';
 
-// TODO: 依存性の整理
-// - PriceRepository()を一度だけインスタンス化
-// - ArbitrageService({ priceRepository })でサービス作成
-// - 各サービスのstart()を呼び出し
 const priceRepository = PriceRepository();
 const exchangeRateRepository = ExchangeRateRepository();
 

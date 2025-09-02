09803738c29d953fb466b3f1eaa2fcea6c867193
lil-shimon <shimon101018@gmail.com>
2025-09-03 08:45:03 +0900

    feat(domain): add triangle arbitrage parameters type definition

--- src/domain/triangle.ts
+++ src/domain/triangle.ts
@@ -1,3 +1,21 @@
-export const calcTriangleArbitrage = () => {
+type Params = {
+  // BTC/USDTなどを想定
+  buyBtcPair: {
+    bid: number;
+    ask: number;
+  };
+  // DOGE/BTCなどを想定
+  buyTokenPair: {
+    bid: number;
+    ask: number;
+  };
+  // DOGE/USDTなどを想定
+  buyStablePair: {
+    bid: number;
+    ask: number;
+  };
+};
+
+export const calcTriangleArbitrage = (params: Params) => {
   // TODO: implement
 };

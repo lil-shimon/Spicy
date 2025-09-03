b590e4d250f9e289aa6bba605ff1abfcfa3595fa
lil-shimon <shimon101018@gmail.com>
2025-09-04 00:08:47 +0900

    index.tsのstart呼び出しに空オブジェクトを渡すよう修正

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ArbitrageController } from './controllers/arbitrage.controller';
 const arbitrageController = ArbitrageController();
 
 const main = async () => {
-  arbitrageController.start();
+  arbitrageController.start({});
 };
 
 main();

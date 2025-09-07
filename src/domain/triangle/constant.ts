4a5dd5177a7fc0fc9d620ac5164b28b04c93a426
lil-shimon <shimon101018@gmail.com>
2025-09-07 17:58:13 +0900

    feat: 三角アービトラージにETH、XMR、XRP、LTC、KCSペアを追加

--- src/domain/triangle/constant.ts
+++ src/domain/triangle/constant.ts
@@ -3,6 +3,11 @@ import { Triangle } from './types';
 export const TRIANGLES: Triangle[] = [
   { name: 'DOGE', base: 'BTC-USDT', mid: 'DOGE-BTC', out: 'DOGE-USDT' },
   { name: 'ADA', base: 'BTC-USDT', mid: 'ADA-BTC', out: 'ADA-USDT' },
+  { name: 'ETH', base: 'BTC-USDT', mid: 'ETH-BTC', out: 'ETH-USDT' },
+  { name: 'XMR', base: 'BTC-USDT', mid: 'XMR-BTC', out: 'XMR-USDT' },
+  { name: 'XRP', base: 'BTC-USDT', mid: 'XRP-BTC', out: 'XRP-USDT' },
+  { name: 'LTC', base: 'BTC-USDT', mid: 'LTC-BTC', out: 'LTC-USDT' },
+  { name: 'KCS', base: 'BTC-USDT', mid: 'KCS-BTC', out: 'KCS-USDT' },
 ];
 
 export const ALL_PAIRS = Array.from(

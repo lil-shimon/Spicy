a52bba63638d94a75849f2f2b499dd935a839d68
lil-shimon <shimon101018@gmail.com>
2025-09-07 14:32:24 +0900

    fix: ペア区切り文字をスラッシュからハイフンに変更

--- src/domain/triangle/constant.ts
+++ src/domain/triangle/constant.ts
@@ -1,6 +1,6 @@
 import { Triangle } from './types';
 
 export const TRIANGLES: Triangle[] = [
-  { name: 'DOGE', base: 'BTC/USDT', mid: 'DOGE/BTC', out: 'DOGE/USDT' },
-  { name: 'ADA', base: 'BTC/USDT', mid: 'ADA/BTC', out: 'ADA/USDT' },
+  { name: 'DOGE', base: 'BTC-USDT', mid: 'DOGE-BTC', out: 'DOGE-USDT' },
+  { name: 'ADA', base: 'BTC-USDT', mid: 'ADA-BTC', out: 'ADA-USDT' },
 ];

c198e355141b10a1281ba82cee62a894c3bef60c
lil-shimon <shimon101018@gmail.com>
2025-09-14 01:25:13 +0900

    feat: エラーハンドリングとテスト実行を追加

--- src/clients/gmo/forex/fetch-forex.client.ts
+++ src/clients/gmo/forex/fetch-forex.client.ts
@@ -2,7 +2,14 @@ const endPoint = 'https://forex-api.coin.z.com/public';
 const path = '/v1/ticker';
 
 export const fetchForex = async () => {
-  const url = `${endPoint}${path}`;
-  const response = await fetch(url);
-  return response.json();
+  try {
+    const url = `${endPoint}${path}`;
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    console.error('GMOのFX価格取得に失敗しました', error);
+    return null;
+  }
 };
+
+fetchForex();

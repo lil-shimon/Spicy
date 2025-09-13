16580e3b9a72daa45a6493a46f770a6cfb2c92c8
lil-shimon <shimon101018@gmail.com>
2025-09-14 01:28:57 +0900

    feat: USD/JPYの価格情報取得処理を実装

--- src/clients/gmo/forex/fetch-forex.client.ts
+++ src/clients/gmo/forex/fetch-forex.client.ts
@@ -2,6 +2,8 @@ const endPoint = 'https://forex-api.coin.z.com/public';
 const path = '/v1/ticker';
 
 export const fetchForex = async () => {
+  const symbol = 'USD_JPY';
+
   try {
     const url = `${endPoint}${path}`;
     const response = await fetch(url);
@@ -9,7 +11,16 @@ export const fetchForex = async () => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const { data } = await response.json();
-    console.log('GMOのFX価格データ:', data);
+
+    const priceInfo = data.find(
+      (item: { symbol: string }) => item.symbol === symbol
+    );
+
+    if (!priceInfo) {
+      throw new Error(`指定されたシンボルのデータが見つかりません: ${symbol}`);
+    }
+
+    console.log('USD/JPYの価格情報:', priceInfo);
   } catch (error) {
     console.error('GMOのFX価格取得に失敗しました', error);
     return null;

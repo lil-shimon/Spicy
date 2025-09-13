572ec35628fed0035ee759eadf6a5176ca261a59
lil-shimon <shimon101018@gmail.com>
2025-09-14 01:24:48 +0900

    feat: fetchForex関数の実装を追加

--- src/clients/gmo/forex/fetch-forex.client.ts
+++ src/clients/gmo/forex/fetch-forex.client.ts
@@ -1,4 +1,8 @@
 const endPoint = 'https://forex-api.coin.z.com/public';
 const path = '/v1/ticker';
 
-export const fetchForex = () => {};
+export const fetchForex = async () => {
+  const url = `${endPoint}${path}`;
+  const response = await fetch(url);
+  return response.json();
+};

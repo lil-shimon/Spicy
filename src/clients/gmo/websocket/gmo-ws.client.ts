9c8041e757eef9fce9947aee01cd5153e0d5ee13
lil-shimon <shimon101018@gmail.com>
2025-09-14 12:39:59 +0900

    feat: GMO WebSocketエンドポイントURLを設定

--- src/clients/gmo/websocket/gmo-ws.client.ts
+++ src/clients/gmo/websocket/gmo-ws.client.ts
@@ -1,7 +1,8 @@
 import { WebSocket } from 'ws';
 
+const endpoint = 'wss://api.coin.z.com/ws/public/v1';
+
 export const connectGmo = () => {
-  const endpoint = '';
   const ws = new WebSocket(endpoint);
 
   return ws;

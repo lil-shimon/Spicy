7ae22988282b6f6c816c2df89b589e2c4b42812c
lil-shimon <shimon101018@gmail.com>
2025-09-15 12:02:46 +0900

    feat: メインエントリーポイントに為替レートアービトラージの起動処理を追加

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { ArbitrageController } from './controllers/arbitrage.controller';
 
 const arbitrageController = ArbitrageController();
 
+const EXCHANGE_RATE_ARBITRAGE_ENABLED: string = 'true';
+const TRIANGLE_ARBITRAGE_ENABLED: string = 'false';
+
 const main = async () => {
-  arbitrageController.start({});
+  if (TRIANGLE_ARBITRAGE_ENABLED === 'true') {
+    arbitrageController.start({});
+  }
+  if (EXCHANGE_RATE_ARBITRAGE_ENABLED === 'true') {
+    arbitrageController.exchangeRateArbitrageStart();
+  }
 };
 
 main();

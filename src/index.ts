0e45aa309a7ca81f2789d54d7c975a3615865232
lil-shimon <shimon101018@gmail.com>
2025-09-15 12:33:46 +0900

    feat: アービトラージ機能の環境変数制御とログ出力を追加

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import { ArbitrageController } from './controllers/arbitrage.controller';
 
 const arbitrageController = ArbitrageController();
 
-const EXCHANGE_RATE_ARBITRAGE_ENABLED: string = 'true';
-const TRIANGLE_ARBITRAGE_ENABLED: string = 'false';
+const EXCHANGE_RATE_ARBITRAGE_ENABLED: string =
+  process.env.EXCHANGE_RATE_ARBITRAGE_ENABLED || 'false';
+const TRIANGLE_ARBITRAGE_ENABLED: string =
+  process.env.TRIANGLE_ARBITRAGE_ENABLED || 'false';
 
 const main = async () => {
   if (TRIANGLE_ARBITRAGE_ENABLED === 'true') {
+    console.log('三角アビトラを開始します');
     arbitrageController.start({});
   }
   if (EXCHANGE_RATE_ARBITRAGE_ENABLED === 'true') {
+    console.log('為替アビトラを開始します');
     arbitrageController.exchangeRateArbitrageStart();
   }
 };

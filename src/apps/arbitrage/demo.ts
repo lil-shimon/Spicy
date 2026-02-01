860b4591705c0e18fe63fb2f8f3eda54af9cb0eb
lil-shimon <shimon101018@gmail.com>
2026-02-01 11:35:46 +0900

    refactor: arbitrageアプリへファイル構造を移動

---
+++ src/apps/arbitrage/demo.ts
@@ -0,0 +1,14 @@
+import { kucoinFuturesClient } from './clients/kucoin/kucoin-client';
+
+const startDemo = async () => {
+  // await startDirtyWork('SOL/USDT', 0.001)
+  // await startDirtyWork('SOL/USDC', 0.001)
+  // await startDirtyWork('ADA/USDC', 1)
+  // await startDirtyWork('ADA/USDT', 1)
+  // await startDirtyWork('PUMP/USDT', 2000, 0.2)
+  await kucoinFuturesClient.loadMarkets();
+  const market = await kucoinFuturesClient.market('PUMPUSDTM');
+  console.log('Market:', market);
+};
+
+startDemo();

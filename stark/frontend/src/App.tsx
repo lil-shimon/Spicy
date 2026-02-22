4cca690051d78368b23d60bb41e82bd5dd8c3cb7
lil-shimon <shimon101018@gmail.com>
2026-02-22 19:56:51 +0900

    update

--- stark/frontend/src/App.tsx
+++ stark/frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useRef } from 'react';
+import { CandlestickSeries, createChart } from 'lightweight-charts';
+
 function App() {
   const url = 'ws://localhost:8080/ws';
   const ws = new WebSocket(url);
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const chart = createChart(containerRef.current);
+    const series = chart.addSeries(CandlestickSeries);
+    return () => chart.remove();
+  }, []);
 
   ws.onopen = () => {
     console.log('WebSocket connection established');
@@ -9,6 +19,7 @@ function App() {
   return (
     <div>
       <h1>stark</h1>
+      <div ref={containerRef} style={{ width: '100%', height: 600 }}></div>
     </div>
   );
 }

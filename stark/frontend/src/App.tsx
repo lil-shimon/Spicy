612ad809083dc0791b86a0fd2fad9395f6dd9f2b
lil-shimon <shimon101018@gmail.com>
2026-02-22 19:38:03 +0900

    update

--- stark/frontend/src/App.tsx
+++ stark/frontend/src/App.tsx
@@ -1,4 +1,11 @@
 function App() {
+  const url = 'ws://localhost:8080/ws';
+  const ws = new WebSocket(url);
+
+  ws.onopen = () => {
+    console.log('WebSocket connection established');
+  };
+
   return (
     <div>
       <h1>stark</h1>
